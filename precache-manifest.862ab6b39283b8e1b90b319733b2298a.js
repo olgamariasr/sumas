a769e447fdccf3958fded0de7144fc24d7ca9e83
olgamariasr <olga.maria.sr@gmail.com>
2020-05-02 14:14:54 +0200

    Updates

---
+++ precache-manifest.862ab6b39283b8e1b90b319733b2298a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "08ea1259750ea8947f0e",
+    "url": "/olgamariasr/sumas/static/css/main.b1c71052.chunk.css"
+  },
+  {
+    "revision": "08ea1259750ea8947f0e",
+    "url": "/olgamariasr/sumas/static/js/main.734f927c.chunk.js"
+  },
+  {
+    "revision": "97b6b336d30c34cc1c8c",
+    "url": "/olgamariasr/sumas/static/js/runtime~main.35559efd.js"
+  },
+  {
+    "revision": "de3d4a121df60fe563f7",
+    "url": "/olgamariasr/sumas/static/css/2.126b3bd3.chunk.css"
+  },
+  {
+    "revision": "de3d4a121df60fe563f7",
+    "url": "/olgamariasr/sumas/static/js/2.216510d4.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/olgamariasr/sumas/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/olgamariasr/sumas/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/olgamariasr/sumas/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/olgamariasr/sumas/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/olgamariasr/sumas/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "2a784db8fa1c041d45c0d67237d978cf",
+    "url": "/olgamariasr/sumas/index.html"
+  }
+];
